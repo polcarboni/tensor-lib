56a8bab5b8b8ccae7d6a351f53b910af88e194b7
polcarboni <paolo_carboni@outlook.it>
2026-01-28 16:31:59 +0100

    Neural network module created

--- src/lib/neural_net/Module.hpp
+++ src/lib/neural_net/Module.hpp
@@ -2,24 +2,27 @@
 #include <vector>
 #include "Tensor.hpp"
 
-template <typename T>
-class Module
+namespace tensor
 {
-protected:
-    bool is_training = ture;
-public:
-    virtual ~Module() = default;
-    virtual Tensor<T> forward(const Tensor<T>& input) = 0;
-    
-    // Pointers to w and b. (for the optimizer) 
-    virtual std::vector<Tensor<T>*> parameters() = 0;
-
-    virtual void train() { is_training = true; }
-    virtual void eval() { is_training = false; }
-
-    // Forward operator overload
-    Tensor<T> operator()(const Tensor<T>& input)
+    template <typename T>
+    class Module
     {
-        return forward(input);
-    }
-};
+    protected:
+        bool is_training = ture;
+    public:
+        virtual ~Module() = default;
+        virtual Tensor<T> forward(const Tensor<T>& input) = 0;
+        
+        // Pointers to w and b. (for the optimizer) 
+        virtual std::vector<Tensor<T>*> parameters() = 0;
+
+        virtual void train() { is_training = true; }
+        virtual void eval() { is_training = false; }
+
+        // Forward operator overload
+        Tensor<T> operator()(const Tensor<T>& input)
+        {
+            return forward(input);
+        }
+    };
+}
--- src/lib/neural_net/ReLU.hpp
+++ src/lib/neural_net/ReLU.hpp
@@ -2,22 +2,25 @@
 #include <algorithm>
 #include "Module.hpp"
 
-template <typename T>
-class ReLU : public Module<T>
+namespace tensor 
 {
-public:
-    ReLU() = default;
-
-    Tensor<T> forward(const Tensor<T>& input) override
+    template <typename T>
+    class ReLU : public Module<T>
     {
-        // Copy shape/metadata(?) from the input Tensor
-        
-        // Application of reLU to all elements:
-        // std::max(static_cast<T>(0), val);
-    }
+    public:
+        ReLU() = default;
 
-    std::vector<Tensor<T>*> parameters() override
-    {
-        return { };
-    }
-};
+        Tensor<T> forward(const Tensor<T>& input) override
+        {
+            // Copy shape/metadata(?) from the input Tensor
+            
+            // Application of reLU to all elements:
+            // std::max(static_cast<T>(0), val);
+        }
+
+        std::vector<Tensor<T>*> parameters() override
+        {
+            return { };
+        }
+    };
+}
