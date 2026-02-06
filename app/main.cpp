a4c0c3f6b783b9635f540cf2d9b56c0d72913111
polcarboni <paolo_carboni@outlook.it>
2026-02-06 19:05:20 +0100

    basic main library linking

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,12 @@
 #include "TensorLib.hpp"
+#include <iostream>
 
 int main()
 {
+    auto t1 = tensor::ScalarType::Float32;
+    auto t2 = tensor::ScalarType::Float64;
+    
+    std::cout << "Promoted type: " << promote_types(t1, t2) << std::endl;
+
     return 0;
 }
