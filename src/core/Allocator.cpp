f785fb752d84170f9dc44c659fcbc38e1f18c249
polcarboni <paolo_carboni@outlook.it>
2026-02-10 14:47:16 +0100

    Implemented functionalities

--- src/core/Allocator.cpp
+++ src/core/Allocator.cpp
@@ -1,12 +1,61 @@
 #include "core/Allocator.hpp"
 #include <cstdlib>
+#include <stdexcept>
+
+#ifdef USE_CUDA
+#include <cuda_runtime.h>
+#endif
 
 namespace tensor {
     
-    void* CPUAllocator::allocate(size_t n) {}
-    void CPUAllocator::deallocate(void* p) {}
+    // -------------------------------------------------------------------------------------------------------------  
+    //                                                 CPU ALLOCATOR 
+    // -------------------------------------------------------------------------------------------------------------  
+    
+    void* CPUAllocator::allocate(size_t n)
+    {
+        if (n == 0) return nullptr;
+        void* ptr = ::operator new(n, std::align_val_t(ALIGNMENT));
+        if (!ptr) throw std::bad_alloc();
+        return ptr;
+    }
 
-    void* CUDAAllocator::allocate(size_t n) {}
-    void CUDAAllocator::deallocate(void* p) {}
+    void CPUAllocator::deallocate(void* p)
+    {
+        if (!p) return;
+        ::operator delete(p, std::align_val_t(ALIGNMENT));
+    }
+    
+
+    
+    // -------------------------------------------------------------------------------------------------------------  
+    //                                                CUDA ALLOCATOR 
+    // -------------------------------------------------------------------------------------------------------------  
+
+    void* CUDAAllocator::allocate(size_t n)
+    {
+        #ifdef USE_CUDA
+            if (n == 0) return nullptr;
+            void* ptr = nullptr;
+            cudaError_t = cudaMalloc(&ptr, n);
+            if(err != cudaSuccess)
+                throw std::runtime_error("CUDA allocation failed");
+            return ptr;
+        #else
+            throw std::runtime_error("CUDAAllocator requires CUDA compilation");
+        #endif
+    }
+
+    void CUDAAllocator::deallocate(void* p)
+    {
+        #ifdef USE_CUDA
+        if(!p) return;
+            cudaError_t err = cudaFree(p);
+            if(err != cudaSuccess)
+                throw std::runtime_error("CUDA deallocation failed");
+        #else
+            throw std::runtime_error("CUDAAllocator requires CUDA compilation");
+        #endif
+    }
 
 } // namespace tensor
--- src/core/Storage.cpp
+++ src/core/Storage.cpp
@@ -1,60 +1,147 @@
 #include "core/Storage.hpp"
 #include "core/Allocator.hpp"
-#include <cstring>  // for std::memcpy if needed
+#include <cstring>
+
+#ifdef USE_CUDA
+#include <cuda_runtime.h>
+#endif
 
 namespace tensor {
 
     // -------------------- PRIVATE HELPERS -------------------- 
 
     Allocator* Storage::get_allocator(DeviceType type) {
-        // Placeholder: return appropriate allocator based on DeviceType
-        return nullptr;
+        
+        if (type == DeviceType::CPU) {
+            static CPUAllocator cpu_instance;
+            return &cpu_instance;
+        } else if (type == DeviceType::CUDA) {
+            static CUDAAllocator cuda_instance;
+            return &cuda_instance;
+        } else {
+            throw std::runtime_error("Unsupported device or CUDA support not copmiled");
+        }
     }
 
+
     void Storage::copy_data_from(const void* src) {
-        // Placeholder: copy data from src to data_
+        
+        if(!src || size_bytes_ == 0 || !data_) return;
+
+        if (!data_) {
+            data_ = get_allocator(device_.type)->allocate(size_bytes_);
+        }
+
+        DeviceType src_type = DeviceType::CPU;
+
+        #ifdef USE_CUDA
+            cudaPointerAttributes attr{};
+            cudaError_t status = cudaPointerGetAttributes(&attr, src);
+
+            if (status != cudaSuccess) {
+                cudaGetLastError();
+            } else {
+                #if CUDART_VERSION >= 10000
+                    if (attr.type == cudaMemoryTypeDevice || attr.type == cudaMemoryTypeManaged)
+                #else 
+                    if (attr.memoryType == cudaMemoryTypeDevice)
+                #endif
+                        src_type = DeviceType::CUDA;
+            }
+            cudaGetLastError();
+        #endif
+
+        if (device_.type == DeviceType::CPU)
+        {
+            if(src_type == DeviceType::CPU) {
+                std::memcpy(data_, src, size_bytes_);       /* CPU -> CPU */
+            } else {
+                #ifdef USE_CUDA
+                    cudaMemcpy(data_, src, size_bytes_, cudaMemcpyDeviceToHost);    /* GPU -> CPU */
+                #endif
+            }
+        } else if (device_.type == DeviceType::CUDA) {
+            #ifdef USE_CUDA
+                cudaMemcpyKind kind = (src_type == DeviceType::CUDA) ? cudaMemcpyDeviceToDevice : cudaMemcpyHostToDevice;
+                cudaError_t err = cudaMemcpy(data_, src, size_bytes_, kind);    /* CPU/GPU -> GPU */
+                if (err != cudaSuccess) {
+                    throw std::runtime_error(cudaGetErrorString(err));
+                }
+            #endif
+            
+        }
     }
 
     // -------------------- CONSTRUCTORS / DESTRUCTOR -------------------- 
 
+    // Empty constructor
     Storage::Storage(size_t size_bytes, Device device)
-        : size_bytes_(size_bytes), device_(device) {
-        // Placeholder: allocate data
+        : size_bytes_(size_bytes), device_(device), data_(nullptr) {
+        
+        if (size_bytes_ > 0) {
+            data_ = get_allocator(device_.type)->allocate(size_bytes_);
+        }
     }
 
+    // Constructor from data pointer
     Storage::Storage(size_t size_bytes, Device device, const void* src)
-        : size_bytes_(size_bytes), device_(device) {
-        // Placeholder: allocate and copy data from src
+        : size_bytes_(size_bytes), device_(device), data_(nullptr) {
+        
+        this->data_ = get_allocator(device_.type)->allocate(size_bytes_);
+        this->copy_data_from(src);
     }
 
+    // Desctructor
     Storage::~Storage() {
-        // Placeholder: deallocate data_
+        if (data_) {
+            get_allocator(device_.type)->deallocate(data_);
+            data_ = nullptr;
+        }
     }
 
+    // Copy constructor
     Storage::Storage(const Storage& other)
-        : size_bytes_(other.size_bytes_), device_(other.device_) {
-        // Placeholder: copy other's data
+        : size_bytes_(other.size_bytes_), device_(other.device_), data_(nullptr) {
+            data_ = get_allocator(device_.type)->allocate(size_bytes_);
+            copy_data_from(other.data_);
     }
 
+    // Copy assignment operator
     Storage& Storage::operator=(const Storage& other) {
         if (this != &other) {
-            // Placeholder: deallocate existing data, copy from other
+            if (data_) {
+                get_allocator(device_.type)->deallocate(data_);
+            }
+            size_bytes_ = other.size_bytes_;
+            device_ = other.device_;
+            data_ = nullptr;
+        }
+
+        if (size_bytes_ > 0) {
+            data_ = get_allocator(device_.type)->allocate(size_bytes_);
+            copy_data_from(other.data_);
         }
         return *this;
     }
 
+    // Move constructor
     Storage::Storage(Storage&& other) noexcept
         : data_(other.data_), size_bytes_(other.size_bytes_), device_(other.device_) {
         other.data_ = nullptr;
         other.size_bytes_ = 0;
     }
 
+    // Move assignment operator
     Storage& Storage::operator=(Storage&& other) noexcept {
         if (this != &other) {
-            // Placeholder: deallocate existing data
+            if (data_) {
+                get_allocator(device_.type)->deallocate(data_);
+            }
+
             data_ = other.data_;
             size_bytes_ = other.size_bytes_;
             device_ = other.device_;
+            
             other.data_ = nullptr;
             other.size_bytes_ = 0;
         }
@@ -62,8 +149,7 @@ namespace tensor {
     }
 
     Storage Storage::clone() const {
-        // Placeholder: return a copy of this storage
-        return Storage(*this);
+        return *this;
     }
 
     // -------------------- ACCESSORS -------------------- 
