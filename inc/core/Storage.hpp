ffa825e68287a1807d79ce57cd052d12119d70d2
polcarboni <paolo_carboni@outlook.it>
2026-02-16 10:48:37 +0100

    fix: forward declaration, printing utilities

--- inc/core/Storage.hpp
+++ inc/core/Storage.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include "Types.hpp"
-#include "Allocator.hpp"
 
 namespace tensor
 {
+    
+    class Allocator;
+
     // -------------------------------------------------------------------------------------------------------------  
     //                                                  STORAGE CLASS
     // ------------------------------------------------------------------------------------------------------------- 
@@ -24,11 +26,12 @@ namespace tensor
         void copy_data_from(const void * src);      /* to be called only after the allocation */
 
     public:
-
+        
+        Storage();
+        ~Storage();
         Storage(size_t size_bytes, Device device);                      /* Uninitialized storage constructor (only allocates space) */
         Storage(size_t size_bytes, Device device, const void* src);     /* Constructor with initialization from host data */
         Storage(const Storage& other);                                  /* Copy constructor */
-        ~Storage();
 
         Storage& operator=(const Storage& other);       /* Copy assignment */
         Storage(Storage&& other) noexcept;              /* Move constructor */
@@ -37,10 +40,33 @@ namespace tensor
         Storage clone() const;
   
         // -------------------- ACCESSORS -------------------- 
-
+        
         void* data() const;
         size_t nbytes() const;
         Device device() const;
+
+        
+        // -------------------- INITIALIZATION/FILLING -------------------- 
+        
+        // template <typename T>
+        // void fill_from_value(T value);
+        
+        // void fill_from_buffer(void *src);
+        
+        // template <typename T>  // provide a function  for generating values
+        // void generate();
     };
+    
+    // -------------------- VISUALIZATION -------------------- 
+    inline std::string to_string(const Storage& storage)
+    {
+        return "Storage (size_bytes=" + std::to_string(storage.nbytes()) +
+        ", device=" + to_string(storage.device()) + ")";
+    }
+
+    inline std::ostream& operator<<(std::ostream& os, const Storage& storage)
+    {
+        return os << to_string(storage);
+    }
 
 } // namespace tensor
