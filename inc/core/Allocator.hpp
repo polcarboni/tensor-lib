cb6951a4641b338ac26ed6ab015d44ae425f187b
polcarboni <paolo_carboni@outlook.it>
2026-02-16 10:53:10 +0100

    fix: added noexcept to deallocators

--- inc/core/Allocator.hpp
+++ inc/core/Allocator.hpp
@@ -12,7 +12,7 @@ namespace tensor {
     class Allocator {
     public:
         virtual void* allocate(size_t num_bytes) = 0;
-        virtual void deallocate(void* ptr) = 0;
+        virtual void deallocate(void* ptr) noexcept = 0;
         virtual ~Allocator() = default;
     };
 
@@ -22,14 +22,14 @@ namespace tensor {
     public:
         static constexpr size_t ALIGNMENT = 64;  //AVX-512/SIMD
         void* allocate(size_t n) override;
-        void deallocate(void* p) override;
+        void deallocate(void* p) noexcept override;
     };
 
     /* CUDA GPU allocator */
     class CUDAAllocator : public Allocator {
     public:
         void* allocate(size_t n) override;
-        void deallocate(void* p) override;
+        void deallocate(void* p) noexcept override;
     };
 
 } // namespace tensor
