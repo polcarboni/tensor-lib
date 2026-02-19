fda34ecf70402e4b973aa8215220df3f38f3ac44
polcarboni <paolo_carboni@outlook.it>
2026-02-19 11:51:18 +0100

    created: dispatchers logic header file

---
+++ inc/core/Dispatchers.hpp
@@ -0,0 +1,122 @@
+#pragma once
+#include "Types.hpp"
+#include "TensorIterator.hpp"
+
+namespace tensor
+{
+    struct TensorImpl;
+}
+
+// Check how inplace operations affect the dispatchers 
+// Check the dispatchers interfaces (easy to do when using them in TensorImpl calls)
+
+namespace tensor::ops
+{
+
+    template <typename Op, typename... Args>
+    void dispatch_impl_(TensorIterator& iter, Args... args)
+    {
+        DISPATCH_ALL_TYPES(iter->get_common_dtype(), "dispatch", [&]
+        {
+            if (iter->get_common_device() == DeviceType::CPU) {
+                Op::template cpu<scalar_t>(iter, args...);
+            }
+            #ifdef USE_CUDA
+            else if (iter->get_common_device() == DeviceType::CUDA) {
+                auto stream = get_current_cuda_stream();
+                Op::template cuda<scalar_t>(iter, stream, args...);
+            }
+            #endif
+            else {
+                throw std::runtime_error("nullary_dispatch: Unsupported device " + to_string(device));
+            }
+        });
+    }
+
+    template <typename Op, typename... Args>
+    tensor::TensorImpl dispatch_nullary(Device device, ScalarType dtype, std::vector<size_t>& shape, Args... args)
+    {
+        TensorIterator iter;
+        iter.build<Op>(shape);
+        dispatch_impl_<Op>(iter, args...);
+
+        return iter.get_output();
+    }
+
+    
+    template <typename Op, typename... Args>
+    tensor::TensorImpl dispatch_unary(Device device, ScalarType dtype, TensorImpl& in, Args... args);
+    // {
+    //     // TensorIterator iter;
+    //     // iter.add_input(in);
+    //     // iter.build<Op>();
+
+    //     // dispatch_impl_<Op>(device, dtype, iter, args...);
+    //     // return iter.get_output();
+    // }
+    
+
+    template <typename Op, typename... Args>
+    tensor::TensorImpl dispatch_unary_inplace(Device device, ScalarType dtype, TensorImpl& in, Args... args);
+    // {   
+    //     // TensorIterator iter;
+    //     // iter.add_output(in);
+    //     // iter.add_input(in);
+    //     // iter.build<Op>();
+
+    //     // dispatch_impl_<Op>(iter, args...);
+    //     // return iter.get_output();
+    // }
+
+    template <typename Op, typename... Args>
+    tensor::TensorImpl dispatch_unary_casting(Device device, ScalarType dtype, TensorImpl& in, Args... args);
+    // {   
+    //     // TensorIterator iter;
+    //     // iter.add_input(in);
+    //     // iter.build<Op>(dtype);
+
+    //     // dispatch_impl_<Op>(iter, args...);
+    //     // return iter.get_output();
+    // }
+    
+
+    template <typename Op, typename... Args>
+    tensor::TensorImpl dispatch_binary(Device device, ScalarType dtype,
+        tensor::TensorImpl& lhs, tensor::TensorImpl& rhs, Args... args)
+    {
+        TensorIterator iter;
+        iter.add_input(lhs);
+        iter.add_input(rhs);
+        iter.build<Op>();
+
+        dispatch_impl_<Op>(iter, args...);
+        return iter.get_output();
+    }
+
+    template <typename BackwardOp, typename... Args>
+    void dispatch_binary_backward(Device device, ScalarType dtype,
+        tensor::TensorImpl& output, tensor::TensorImpl& lhs, tensor::TensorImpl& rhs,
+        Args... args)
+    {
+        TensorIterator iter;
+        iter.add_input(output.autograd_meta_.grad_);    //Upstream grad
+        iter.add_input(lhs);
+        iter.add_input(rhs);
+        iter.add_output(lhs.autograd_meta_.grad_);
+        iter.add_output(rhs.autograd_meta_.grad_);
+        iter.build<BackwardOp>(device, dtype);
+
+        dispatch_impl_<BackwardOp>(device, dtype, iter, args...);
+        return iter.get_outputs();  // Return both output tensors?
+    }
+    
+    template <typename Op, typename... Args>
+    void dispatch_ternary(Device device, ScalarType dtype, TensorImpl* a, TensorImpl* b, TensorImpl* c, Args... args);
+    
+    template <typename Op, typename... Args>
+    void dispatch_comparison(Device device, ScalarType dtype, TensorImpl* lhs, TensorImpl* rhs, Args... args);
+    
+    template <typename Op, typename... Args>
+    void dispatch_reduction(Device device, ScalarType dtype, TensorImpl* tensor, Args... args);
+
+} // namespace tensor::ops
