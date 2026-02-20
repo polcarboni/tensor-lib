aaa14585c2172fc13b0e0411546a232ffea0810c
polcarboni <paolo_carboni@outlook.it>
2026-02-20 11:07:21 +0100

    added: unchecked FillConst contiguous path

--- inc/ops/FillStorageOps.hpp
+++ inc/ops/FillStorageOps.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "../core/TensorIterator.hpp"
+#include "core/TensorIterator.hpp"
 #include <algorithm>
 #include <cstdint>
 
@@ -28,50 +28,65 @@ namespace tensor::ops
 
     struct FillConst : FillOpBase {
         template <typename T>
-        static void cpu(TensorIterator& iter, T value);
+        static void cpu(tensor::TensorIterator& iter, T value)
+        {
+            assert(get_scalar_type<T>() == iter->get_common_dtype());
+
+            T* output = iter.output_ptr<T>(0);
+
+            auto& outputs = iter.get_outputs();
+            size_t total_size = outputs[0]->get_total_size();
+
+            if (outputs[0]->get_contiguous()) {
+                // Fast path for contiguous tensor
+                std::fill(output, output + total_size, value);
+            } else {
+                throw std::runtime_error("FILLCONSTOP FOR NON CONTIGUOUS STILL NOT IMPLEMENTED");
+            }
+        }
         
         template <typename T>
-        static void cuda(TensorIterator& iter, cudaStream_t stream = nullptr, T value = T{0});
+        static void cuda(tensor::TensorIterator& iter, cudaStream_t stream = nullptr, T value = T{0});
     };
     
     struct FillBuffer : FillOpBase  {
         template <typename T>
-        static void cpu(TensorIterator& iter, const T* src);
+        static void cpu(tensor::TensorIterator& iter, const T* src);
         
         template <typename T>
-        static void cuda(TensorIterator& iter, cudaStream_t stream, const T* src);
+        static void cuda(tensor::TensorIterator& iter, cudaStream_t stream, const T* src);
     };
 
     struct FillArange : FillOpBase  {
         template <typename T>
-        static void cpu(TensorIterator& iter, T start, T step);
+        static void cpu(tensor::TensorIterator& iter, T start, T step);
         
         template <typename T>
-        static void cuda(TensorIterator& iter, cudaStream_t stream, T start, T step);
+        static void cuda(tensor::TensorIterator& iter, cudaStream_t stream, T start, T step);
     };
 
     struct FillLinspace : FillOpBase  {
         template <typename T>
-        static void cpu(TensorIterator& iter, T start, T step);
+        static void cpu(tensor::TensorIterator& iter, T start, T step);
         
         template <typename T>
-        static void cuda(TensorIterator& iter, cudaStream_t stream = nullptr, T start = T{0}, T step = T{1});
+        static void cuda(tensor::TensorIterator& iter, cudaStream_t stream = nullptr, T start = T{0}, T step = T{1});
     };
 
     struct FillRandomUniform : FillOpBase  {
         template <typename T>
-        static void cpu(TensorIterator& iter, T low = T{0}, T high = T{1}, uint64_t seed = 0);
+        static void cpu(tensor::TensorIterator& iter, T low = T{0}, T high = T{1}, uint64_t seed = 0);
         
         template <typename T>
-        static void cuda(TensorIterator& iter, cudaStream_t stream = nullptr, T low = T{0}, T high = T{1}, uint64_t seed = 0);
+        static void cuda(tensor::TensorIterator& iter, cudaStream_t stream = nullptr, T low = T{0}, T high = T{1}, uint64_t seed = 0);
     };
 
     struct FillRandomNormal : FillOpBase  {
         template <typename T>
-        static void cpu(TensorIterator& iter, T mean = T{0}, T stddev = T{1}, uint64_t seed = 0);
+        static void cpu(tensor::TensorIterator& iter, T mean = T{0}, T stddev = T{1}, uint64_t seed = 0);
         
         template <typename T>
-        static void cuda(TensorIterator& iter, cudaStream_t stream = nullptr, T mean = T{0}, T stddev = T{1}, uint64_t seed = 0);
+        static void cuda(tensor::TensorIterator& iter, cudaStream_t stream = nullptr, T mean = T{0}, T stddev = T{1}, uint64_t seed = 0);
     };
 
 } // namespace tensor::ops
