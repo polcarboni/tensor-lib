d35d3519d63bd4ffa3c4e6a7a2216c04fdb45636
polcarboni <paolo_carboni@outlook.it>
2026-02-25 11:28:28 +0100

    commented for avoiding redeclaration compilation error

--- src/autograd/AutogradEngine.cpp
+++ src/autograd/AutogradEngine.cpp
@@ -3,17 +3,17 @@
 
 namespace tensor::grad
 {
-    void AutogradEngine::execute_graph(std::shared_ptr<Node> root_node, TensorImpl initial_grad)
-    {
-        // None
-    }
+    // void AutogradEngine::execute_graph(std::shared_ptr<Node> root_node, TensorImpl initial_grad)
+    // {
+    //     // None
+    // }
 
-    void AutogradEngine::backward(const std::vector<TensorImpl>& roots,
-                          const std::vector<TensorImpl>& grad_outputs,
-                          bool retain_graph,
-                          bool create_graph)
-    {
-        // None
-    }
+    // void AutogradEngine::backward(const std::vector<TensorImpl>& roots,
+    //                       const std::vector<TensorImpl>& grad_outputs,
+    //                       bool retain_graph,
+    //                       bool create_graph)
+    // {
+    //     // None
+    // }
 
 } // namespace tensor::grad
