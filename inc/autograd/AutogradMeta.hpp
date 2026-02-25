0b8ada99fa01d43a99d605629d1fae5cc3cf6851
polcarboni <paolo_carboni@outlook.it>
2026-02-25 10:56:28 +0100

    fix: changed namespace to tensor::grad, comments

--- inc/autograd/AutogradMeta.hpp
+++ inc/autograd/AutogradMeta.hpp
@@ -4,20 +4,22 @@
 #include <memory>
 #include <cstdint>
 
-namespace tensor
+namespace tensor::grad
 {
     // ------------------------------ AUTOGRAD META STRUCT  --------------------------------
     
-    class TensorImpl;
+    class ::tensor::TensorImpl;
     class Node;
     
     struct AutogradMeta {
-        std::shared_ptr<TensorImpl> grad_ = nullptr;    
-        std::shared_ptr<Node> grad_fn_ = nullptr;
+        std::shared_ptr<TensorImpl> grad_ = nullptr;    /* Accumulated gradient */ 
+        std::shared_ptr<Node> grad_fn_ = nullptr;       /* Operation that created the node */
         std::weak_ptr<Node> grad_accumulator_;
         uint32_t version_ = 0;
 
         std::unique_ptr<AutogradMeta> clone() const;
+        
+        bool is_leaf() const { return grad_fn_ == nullptr; }
     };
 
-} // namespace tensor
+} // namespace tensor::grad
