625c2f955bf8639f8d0e5ea735ed433832398910
polcarboni <paolo_carboni@outlook.it>
2026-02-26 14:44:23 +0100

    created: basic matmul ops structs

---
+++ inc/ops/MatmulOps.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include "core/TensorIterator.hpp"
+#include <algorithm>
+#include <cstdint>
+
+// Avoids inclusion of CUDA headers
+typedef struct CUstream_st* cudaStream_t;
+
+namespace tensor::ops
+{
+    
+    template <Direction Dir>
+    class MatmulOpBase {
+    protected:
+        static constexpr bool supports_broadcasting_ = false;
+        static constexpr int num_inputs_ = 2;
+        static constexpr int num_outputs_ = 1;
+        static constexpr IterationKind iter_kind_ = IterationKind::MATMUL;
+
+    public:
+        static constexpr bool supports_broadcasting() { return supports_broadcasting_; }
+        static constexpr int num_inputs()             { return num_inputs_; }
+        static constexpr int num_outputs()            { return num_outputs_; }
+        static constexpr IterationKind iter_kind()    { return iter_kind_; }
+        static constexpr Direction get_direction()    { return Dir; }
+    };
+
+
+    class MatMul : MatmulOpBase<Direction::FORWARD> {
+        static void cpu(TensorIterator& iter);
+        static void cuda(TensorIterator& iter, cudaStream_t stream);
+    };
+
+    /**
+     * Other possible operations
+     * 
+     * MatmulTransposeA or MatmultransposeB: fusion of transpose and matmul
+     * MatmulAdd(linear): fused matmul and bias sum.
+     */
+
+} // namespace tensor::ops
