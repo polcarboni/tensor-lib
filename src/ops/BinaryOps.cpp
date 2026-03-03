9fa73e8ca7fdf9b4048a3bcf2594561b747b7dec
polcarboni <paolo_carboni@outlook.it>
2026-03-03 20:27:49 +0100

    added: generic binary kernel implementation

--- src/ops/BinaryOps.cpp
+++ src/ops/BinaryOps.cpp
@@ -4,29 +4,84 @@
 #include <cstdint>
 #include <cassert>
 
-namespace tensor::ops
-{
-    // Naive implementation (wrong): iterator still not proivdes the strides and ndims
-    // for computation. The used loop is obviously wrong.
-    void BinaryAdd::cpu(TensorIterator& iter) {
-        
+namespace tensor::ops::kernel {
+
+    template <typename Op>
+    void binary_cpu_kernel(TensorIterator& iter, Op op)
+    {
         auto dtype = iter.get_common_dtype();
 
-        DISPATCH_ALL_TYPES(dtype, "binary_add", [&] {
+        std::cout << "BINARY OP: ";
+
+        DISPATCH_ALL_TYPES(dtype, "binary_add", ([&] {
             scalar_t* output    = iter.output_ptr<scalar_t>(0);
             const scalar_t* lhs = iter.input_ptr<scalar_t>(0);
             const scalar_t* rhs = iter.input_ptr<scalar_t>(1);
 
-            size_t total_size = iter.get_outputs()[0]->get_total_size();
+            const auto& lhs_strides = iter.get_strides(0);
+            const auto& rhs_strides = iter.get_strides(1);
+            const auto& out_strides = iter.get_strides(2);
+            
+            size_t numel      = iter.get_numel();
+            const auto& shape = iter.get_shape();
+            size_t ndim       = iter.get_ndim();
 
-            if (iter.get_common_is_contiguous()) {
-                for (size_t i = 0; i < total_size; ++i) {
-                    output[i] = lhs[i] + rhs[i];
+            std::cout << "ndim=[" << ndim << "],  common_contiguous=[" << iter.get_common_is_contiguous() << "]";
+
+            if (iter.get_common_is_contiguous() && ndim == 1) {
+                std::cout << "fast path, " << std::endl;
+                /* Contiguous elements and single dimension coalesced */
+                for (size_t i = 0; i < numel; ++i) {
+                    output[i] = op(lhs[i], rhs[i]);
+                }
+            } else if (ndim == 1) {
+                /* non contiguous, single dimension */
+                std::cout << "1-dim path, " << std::endl;
+                for (size_t i = 0; i < numel; ++i) {
+                    output[i * out_strides[0]] = op(lhs[i * lhs_strides[0]], rhs[i * rhs_strides[0]]);
                 }
             } else {
-                throw std::runtime_error("BINARY_ADD FOR NON CONTIGUOUS STILL NOT IMPLEMENTED");
-            }
-        });
+                /* General path */
+                std::cout << "general path, ";
+                
+                std::vector<size_t> counter(ndim, 0);
+                size_t lhs_offset = 0;
+                size_t rhs_offset = 0;
+                size_t out_offset = 0;
+                
+                std::cout << "numel=[" << numel << "], " << std::endl;
+
+                for (size_t i = 0; i < numel; ++i) {
+                    
+                    output[out_offset] = op(lhs[lhs_offset], rhs[rhs_offset]);
+                    
+                    for (int d = static_cast<int>(ndim) - 1; d >= 0; --d) {
+                        ++counter[d];
+                        lhs_offset += lhs_strides[d];
+                        rhs_offset += rhs_strides[d];
+                        out_offset += out_strides[d];
+
+                        if (counter[d] < shape[d]) break;
+
+                        /* Reset offset and counter when the dimension is computed */
+                        counter[d] = 0;
+                        lhs_offset -= shape[d] * lhs_strides[d];
+                        rhs_offset -= shape[d] * rhs_strides[d];
+                        out_offset -= shape[d] * out_strides[d];
+                    }
+                }
+            }    
+        }));
+    };
+
+}; // namespace tensor::ops::kernel
+
+
+namespace tensor::ops
+{
+
+    void BinaryAdd::cpu(TensorIterator& iter) {
+        kernel::binary_cpu_kernel(iter, [](auto a, auto b) { return a + b; });
     }
     
     void BinaryAddBackward::cpu(TensorIterator& iter) {}
