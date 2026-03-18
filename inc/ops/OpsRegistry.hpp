48df5400579e7b2c8dd357b5c89a78bc4ec9b1e3
polcarboni <paolo_carboni@outlook.it>
2026-03-18 08:21:19 +0100

    chore: add operation to common operations include registry

--- inc/ops/OpsRegistry.hpp
+++ inc/ops/OpsRegistry.hpp
@@ -3,11 +3,11 @@
 #include "FillStorageOps.hpp"
 #define FOR_EACH_FILLSTORAGE_OP(X)  \
     X(FillConst)                    \
-    X(FillBuffer)                   \
     X(FillArange)                   \
     X(FillLinspace)                 \
     X(FillRandomUniform)            \
-    X(FillRandomNormal)             
+    X(FillRandomNormal)             \
+    X(FillEye)
 
 #include "BinaryOps.hpp"
 #define FOR_EACH_BINARY_OP(X)   \
@@ -20,8 +20,21 @@
 #define FOR_EACH_REDUCTION_OP(X) \
     X(ReduceSum)
 
+#include "UnaryOps.hpp"
+#define FOR_EACH_UNARY_OP(X) \
+    X(UnaryCastOp)           \
+    X(ContiguousOp)          \
+    X(UnaryNeg)              \
+    X(UnaryAbs)              \
+    X(UnaryExp)              \
+    X(UnaryLog)              \
+    X(UnarySigmoid)          \
+    X(UnaryTanh)             \
+    X(UnaryRelu)             
+
 
 #define FOR_EACH_OP(X)          \
     FOR_EACH_FILLSTORAGE_OP(X)  \
     FOR_EACH_BINARY_OP(X)       \
-    FOR_EACH_REDUCTION_OP(X)
+    FOR_EACH_REDUCTION_OP(X)    \
+    FOR_EACH_UNARY_OP(X)
